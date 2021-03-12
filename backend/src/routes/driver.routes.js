636a942ddd9a4466037934d640b32fb12f38e074
Baraka Mulumia <barakamulumia@gmail.com>
2021-03-12 10:44:38 +0300

    ^verify ==> sift ^underscore notation

--- backend/src/routes/driver.routes.js
+++ backend/src/routes/driver.routes.js
@@ -1,20 +1,20 @@
 const controller = require("../controllers/driver.controller");
-const { verifyXpressDriver: auth } = require("../middlewares");
+const { sift } = require("../middlewares");
 
 module.exports = function (app) {
   app.get("/api/drivers/all", controller.getAllDrivers);
 
   app.get("/api/drivers/findone", controller.getDriverById);
 
-  app.get("/api/drivers/check-verification", controller.verifyRegistered);
+  app.get("/api/drivers/check-verification", controller.check_approval);
 
   app.post(
     "/api/drivers/complete-registration",
     [
-      auth.checkDuplicateUserId,
-      auth.checkDuplicateDlno,
-      auth.checkDuplicateTruckNo,
+      sift.checkDuplicateUserId,
+      sift.checkDuplicateDlno,
+      sift.checkDuplicateTruckNo,
     ],
-    controller.completeRegistration
+    controller.complete_registration
   );
 };
