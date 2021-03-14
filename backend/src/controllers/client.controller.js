61a984e904eb9da02abbd8540932acef0857cd24
Baraka Mulumia <barakamulumia@gmail.com>
2021-03-14 22:18:12 +0300

    ^async await

--- backend/src/controllers/client.controller.js
+++ backend/src/controllers/client.controller.js
@@ -2,50 +2,17 @@ const db = require("../models");
 const User = db.user;
 const Role = db.role;
 
-exports.getClientById = (req, res) => {
-  User.findOne(
-    {
-      _id: req.headers.clientid,
-    },
-    (err, client) => {
-      if (err) {
-        res.status(200).json({
-          message: err,
-        });
-        return;
-      }
-      if (!client) {
-        res.status(404).json({
-          message: "Clinet not found",
-        });
-        return;
-      }
+module.exports = {
+  get: async (req, res) => {
+    const id = req.headers.clientid;
+    const user = await User.findById(id);
 
-      Role.findOne(
-        {
-          _id: client.role,
-        },
-        (err, role) => {
-          if (err) {
-            res.status(200).json({
-              message: err,
-            });
-            return;
-          }
-          if (!role) {
-            res.status(404).json({
-              message: "Role does not exist",
-            });
-            return;
-          }
-          const { firstname, lastname } = client;
-
-          res.status(200).json({
-            firstname,
-            lastname,
-          });
-        }
-      );
+    if (!user) {
+      res.status(404).json({
+        message: "Client Not Found",
+      });
+      return;
     }
-  );
+    res.status(200).json(user);
+  },
 };
